c8329a3aa797b7ee0ef95c7f19ceee91b9550a3c
rqk <renqiukai@qq.com>
2021-05-30 17:55:58 +0800

    更新外部联系人接口

--- pywxwork/customer/__init__.py
+++ pywxwork/customer/__init__.py
@@ -13,6 +13,84 @@ class customer(base):
         logger.debug(response)
         return response
 
+    def add_contact_way(self, data):
+        """
+        配置客户联系「联系我」方式
+        """
+        api_name = "externalcontact/add_contact_way"
+        response = self.request(
+            api_name=api_name, method="post", json=data)
+        logger.debug(response)
+        return response
+
+    def add_contact_way_demo(self):
+        data = {
+            "type": 1,
+            "scene": 1,
+            "style": 1,
+            "remark": "渠道客户",
+            "skip_verify": True,
+            "state": "teststate",
+            "user": ["zhangsan", "lisi", "wangwu"],
+            "party": [2, 3],
+            "is_temp": True,
+            "expires_in": 86400,
+            "chat_expires_in": 86400,
+            "unionid": "oxTWIuGaIt6gTKsQRLau2M0AAAA",
+            "conclusions":
+            {
+                "text":
+                {
+                    "content": "文本消息内容"
+                },
+                "image":
+                {
+                    "media_id": "MEDIA_ID"
+                },
+                "link":
+                {
+                    "title": "消息标题",
+                    "picurl": "https://example.pic.com/path",
+                    "desc": "消息描述",
+                    "url": "https://example.link.com/path"
+                },
+                "miniprogram":
+                {
+                    "title": "消息标题",
+                    "pic_media_id": "MEDIA_ID",
+                    "appid": "wx8bd80126147dfAAA",
+                    "page": "/path/index.html"
+                }
+            }
+        }
+
+        response = self.add_contact_way(data)
+        logger.debug(response)
+        return response
+
+    def get_contact_way(self, config_id):
+        """
+        获取企业已配置的「联系我」方式
+        """
+        api_name = "externalcontact/get_contact_way"
+        data = {
+            "config_id": config_id
+        }
+        response = self.request(
+            api_name=api_name, method="post", json=data)
+        logger.debug(response)
+        return response
+
+    def update_contact_way(self, data):
+        """
+        更新企业已配置的「联系我」方式
+        """
+        api_name = "externalcontact/update_contact_way"
+        response = self.request(
+            api_name=api_name, method="post", json=data)
+        logger.debug(response)
+        return response
+
     def list(self, userid):
         api_name = "externalcontact/list"
         params = {
---
+++ pywxwork/customer/tag.py
@@ -0,0 +1,89 @@
+from loguru import logger
+from ..base import base
+
+
+class tag(base):
+    def __init__(self, token: str):
+        super().__init__(token)
+
+    def list(self, tag_id: list = [], group_id: list = []):
+        api_name = "externalcontact/get_corp_tag_list"
+        params = {}
+        if tag_id:
+            params["tag_id"] = tag_id
+        if group_id:
+            params["group_id"] = group_id
+        response = self.request(
+            api_name=api_name,
+            json=params,
+            method="POST")
+        logger.debug(response)
+        return response
+
+    def add(self, tag: list, order: int, group_id: int = None, group_name: str = None, agentid: str = None):
+        """添加企业客户标签
+
+        Args:
+            tag (list): [标签]
+            order (int): [标签组次序值。order值大的排序靠前。有效的值范围是[0, 2^32)]
+            group_id (int, optional): [标签组id]. Defaults to None.
+            group_name (str, optional): [标签组名称，最长为30个字符]. Defaults to None.
+            agentid (str, optional): [授权方安装的应用agentid。仅旧的第三方多应用套件需要填此参数]. Defaults to None.
+
+        Returns:
+            [type]: [description]
+
+        Example(tag):
+            {
+                "name": "TAG_NAME_1",
+                "order": 1
+            }
+        """
+        api_name = "externalcontact/add_corp_tag"
+        params = {
+            "order": order,
+            "tag": tag,
+            "agentid": 1000014
+        }
+        if group_name:
+            params["group_name"] = group_name
+        if agentid:
+            params["agentid"] = agentid
+        if group_id:
+            # 有groupid表示更新组内的标签无groupid表示新建组
+            params["group_id"] = group_id
+        response = self.request(
+            api_name=api_name,
+            json=params,
+            method="POST")
+        logger.debug(response)
+        return response
+
+    def update(self,  id: str, name: str = None, order: int = None, agentid: str = None):
+        """编辑企业客户标签
+
+        Args:
+            id (str): 标签或标签组的id
+            name (str, optional): 新的标签或标签组名称，最长为30个字符. Defaults to None.
+            order (int, optional): 	标签/标签组的次序值。order值大的排序靠前。有效的值范围是[0, 2^32). Defaults to None.
+            agentid (str, optional): 	授权方安装的应用agentid。仅旧的第三方多应用套件需要填此参数. Defaults to None.
+
+        Returns:
+            response: dict
+        """
+        api_name = "externalcontact/edit_corp_tag"
+        params = {
+            "id": id,
+        }
+        if name:
+            params["name"] = name
+        if agentid:
+            params["agentid"] = agentid
+        if order:
+            params["order"] = order
+        response = self.request(
+            api_name=api_name,
+            json=params,
+            method="POST")
+        logger.debug(response)
+        return response
